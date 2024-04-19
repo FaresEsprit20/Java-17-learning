fc827c5b53038c1057add97d8c523a1a973590f6
Fares Ben Slama <fares.benslama@esprit.tn>
2024-04-19 19:42:40 +0200

    first commit

--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -1,13 +1,19 @@
 package com.learning;
 
-import java.util.Arrays;
+
+import java.util.Scanner;
 
 /**
  * @author Fares
  */
 
 public class Main {
-  public static void main(String[] args) {
+
+  public static void main(String... args) {
+
+
+
+
 
   }
 
