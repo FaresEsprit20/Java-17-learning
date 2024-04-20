bac27fdd239a560005e55187f606d2db9c8e93af
Fares Ben Slama <fares.benslama@esprit.tn>
2024-04-20 18:37:32 +0200

    first commit

--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -1,7 +1,4 @@
 package com.learning;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
 
 /**
  * @author Fares
@@ -9,14 +6,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String... args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter any number :  ");
+
+    public static void main(String[] args) {
+
     }
-
-
-
-
 
 
 }
--- src/com/learning/procedural_and_intro/methods4/MethodsMain.java
+++ src/com/learning/procedural_and_intro/methods4/MethodsMain.java
@@ -371,4 +371,34 @@ public class MethodsMain {
   }
 
 
+/*
+  public static String convert1(String input) {
+    String result = "";
+    String[] lines = input.split(System.lineSeparator());
+    for (int i = 1; i < lines.length; i++) {
+      String[] wordsInLine = lines[i].split(";");
+      result += wordsInLine[0] + " ==> " + wordsInLine[2] + System.lineSeparator();
+    }
+    return result;
+
+  }
+
+  public static final String INPUT_DATA = "Login;Name;Email" + System.lineSeparator() +
+          "peterson;Chris Peterson;peterson@outlook.com" + System.lineSeparator() +
+          "james;Derek James;james@gmail.com" + System.lineSeparator() +
+          "jackson;Walter Jackson;jackson@gmail.com" + System.lineSeparator() +
+          "gregory;Mike Gregory;gregory@yahoo.com";
+
+  public static String convert2(String input) {
+    String result = new String();
+    String[] lines = input.split(System.lineSeparator());
+    for (int i = 1; i < lines.length; i++) {
+      String[] wordsInLine = lines[i].split(";");
+      result += wordsInLine[1] + " (email: " + wordsInLine[2] + ")" + System.lineSeparator();
+    }
+    return result;
+
+  }*/
+
+
 }
