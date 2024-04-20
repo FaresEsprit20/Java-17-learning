157ba48c669eb696b11519b10d728a10c52562d7
Fares Ben Slama <fares.benslama@esprit.tn>
2024-04-20 04:01:04 +0200

    first commit

--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the length of the Array :  ");
-   
+
     }
 
 
