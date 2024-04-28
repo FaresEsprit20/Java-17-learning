721e6d1f6313743b5352c8190957fade21405c94
Fares Ben Slama <fares.benslama@esprit.tn>
2024-04-28 19:45:17 +0200

    first commit

---
+++ src/com/learning/procedural_and_intro/enums/HomeWork.java
@@ -0,0 +1,25 @@
+package com.learning.procedural_and_intro.enums;
+
+import java.util.Scanner;
+
+public class HomeWork {
+
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n Enter a Message Type :  ");
+        char input = scanner.next().charAt(0);
+//        do {
+//            System.out.println("\n  Message Type Must be : A or B or C or D ");
+//            input = scanner.next().charAt(0);
+//        }while (Character.toUpperCase(input) != 'A' && Character.toUpperCase(input) != 'B' && Character.toUpperCase(input) != 'C' && Character.toUpperCase(input) != 'D' );
+
+        while (Character.toUpperCase(input)  != 'A' && Character.toUpperCase(input) != 'B' && Character.toUpperCase(input) != 'C' && Character.toUpperCase(input) != 'D' ) {
+            System.out.println("\n  Message Type Must be : A or B or C or D ");
+            input = scanner.next().charAt(0);
+        }
+
+    }
+
+
+
+}
---
+++ src/com/learning/procedural_and_intro/enums/Priority.java
@@ -0,0 +1,7 @@
+package com.learning.procedural_and_intro.enums;
+
+public enum Priority {
+    HIGH, MEDIUM, LOW
+
+
+}
