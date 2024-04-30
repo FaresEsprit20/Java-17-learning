ca2ca5c6b6789cf3661e3f276716a8e7e6e0e91b
Fares Ben Slama <fares.benslama@esprit.tn>
2024-04-30 22:00:11 +0200

    first commit

--- src/com/learning/oop/oopMain.java
+++ src/com/learning/oop/oopMain.java
@@ -8,14 +8,14 @@ public class oopMain {
     //Initialization Block
 
     /*
-     * This is only called when an object is created
+     * This is only called when an object is created !
      */
     {
         System.out.println(" \n Initialization Block loaded into JVM \n");
     }
 
     /*
-     * This is only called when a Class is loaded
+     * This is only called when a Class is loaded !
      */
     static {
         System.out.println(" \n Class Main loaded into JVM \n");
